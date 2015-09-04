b5d2457907a83671dc479c9e0a3354b2e6deccd6
Shaun Mulligan <switchflip.shaun@gmail.com>
2015-09-04 11:27:14 +0300

    change to piTFT clock example

---
+++ start.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+udevd --daemon
+udevadm trigger
+
+if [ ! -c /dev/fb1 ]; then
+  modprobe spi-bcm2708
+  modprobe fbtft_device name=pitft verbose=0 rotate=270
+
+  sleep 1
+
+  mknod /dev/fb1 c $(cat /sys/class/graphics/fb1/dev | tr ':' ' ')
+fi
